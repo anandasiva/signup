3a944e4a79777167dacf0817bbb499dd14d11b94
anandasiva <anandasiva4@gmail.com>
2023-04-27 20:32:07 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { groupBy } from 'rxjs/internal/operators/groupBy';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'signup';
+  title = 'SIGNUP';
+  signupForm;
+
+  constructor(private formBuilder: FormBuilder) {
+  
+
+  this.signupForm = this.formBuilder.group({
+     firstname:['',[Validators.required,Validators.minLength(10),Validators.pattern("[A-Z]+")]],
+     lastname:['',[Validators.required,Validators.maxLength(10),Validators.pattern("[A-Z]+")]],
+     gender:['',[Validators.required]],
+     age:['',[Validators.required]],
+     mobilenumber:['',[Validators.required,Validators.pattern("[+91]+[0-9]+")]], 
+     email:['',[Validators.required,Validators.email]],
+     password:['',[Validators.required,Validators.pattern("[A-Z]+[a-z]+[@]+[0-9]+")]],
+     confirmpassword:['',[Validators.required,Validators.pattern("[A-Z]+[a-z]+[@]+[0-9]+")]],    
+  })
+
+  }
+
+  get firstname() {
+    return this.signupForm.get('firstname');
+  }
+  get lastname(){
+    return this.signupForm.get('lastname');
+  }
+  get gender(){
+    return this.signupForm.get('gender');
+  }
+  get age(){
+    return this.signupForm.get("age");
+  }
+  get mobilenumber() {
+    return this.signupForm.get('mobilenumber');
+  }
+  get email(){
+    return this.signupForm.get('email');
+  }
+  get password(){
+    return this.signupForm.get('password');
+  }
+  get confirmpassword(){
+    return this.signupForm.get('confirmpassword');
+  }
+ 
+  onSubmit(){
+    console.log(this.signupForm.value);
+  }
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,10 +8,12 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
